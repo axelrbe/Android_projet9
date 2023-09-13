fbe3d8ff8b3e71cc7d2cf8ed7f398efa1af93b14
axelrbe <ribeiro.axel.fabien@gmail.com>
2023-09-13 11:48:48 +0200

    Add property page design

---
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package com.openclassrooms.realestatemanager
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        Assert.assertEquals("com.openclassrooms.go4lunch", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.openclassrooms.realestatemanager
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.openclassrooms.realestatemanager.Utils.convertDollarToEuro
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
